af6f76637ef47c70c495878871ae4ebb2218df55
Gert-Jan <gertjancom@gmail.com>
2016-07-26 19:01:24 +0200

    Add distance of pokemon to the notification text

--- pogom/alarm/notifications.py
+++ pogom/alarm/notifications.py
@@ -3,6 +3,10 @@ import json
 import logging
 from datetime import datetime
 
+from s2sphere import LatLng
+
+from pogom import config
+
 log = logging.getLogger(__name__)
 
 from pb_alarm import PB_Alarm
@@ -32,8 +36,8 @@ class Notifications:
 						log.info("Alarm type not found: " + alarm['type'])
 				else:
 					log.info("Alarm not activated: " + alarm['type'])
-			
-				
+
+
 	def notify_pkmns(self, pkmn):
 		for id in pkmn:
 			if id not in self.seen:
@@ -45,6 +49,14 @@ class Notifications:
 				}
 				self.seen[id] = pkinfo
 				if(self.notify_list[pkinfo['name']] == "True"):
+					origin_point = LatLng.from_degrees(config['ORIGINAL_LATITUDE'], config['ORIGINAL_LONGITUDE'])
+					pokemon_point = LatLng.from_degrees(pkinfo['lat'], pkinfo['lng'])
+					pkinfo['distance'] = int(origin_point.get_distance(pokemon_point).radians * 6366468.241830914)
+					diff = pokemon_point - origin_point
+					diff_lat = diff.lat().degrees
+					diff_lng = diff.lng().degrees
+					pkinfo['direction'] = (('N' if diff_lat >= 0 else 'S') if abs(diff_lat) > 1e-4 else '') + (
+						('E' if diff_lng >= 0 else 'W') if abs(diff_lng) > 1e-4 else '')
 					log.info(pkinfo['name']+" notification has been triggered!")
 					log.info("Encounter ID:" + str(id))
 					for alarm in self.alarms:
@@ -62,7 +74,6 @@ class Notifications:
 
 	def notify_lures(self, lures):
 		raise NotImplementedError("This method is not yet implimented.")
-		
+
 	def notify_gyms(self, gyms):
 		raise NotImplementedError("This method is not yet implimented.")
-	
